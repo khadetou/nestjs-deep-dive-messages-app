2b421bbcd2a48372526ec3cd26ea99ea1300d100
khadetou <khadetou96@gmail.com>
2021-12-22 17:39:33 +0000

    http request setup

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+  @Get()
+  listMessage() {
+    return 'hello';
+  }
+
+  @Post()
+  createMessage() {}
+
+  @Get('/:id')
+  getMessage() {}
+}
